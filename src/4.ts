b7e1a7fb6791b5f2ea9aa6c5026ee49a05b3cbd6
Olexandra2209 <123394674+Olexandra2209@users.noreply.github.com>
2023-10-19 14:55:39 +0300

    Done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,57 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected tenants: Person[] = [];
+  protected key: Key;
+
+  constructor(key: Key) {
+    this.door = false;
+    this.tenants = [];
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is open.");
+    } else {
+      console.log("The key doesn't fit");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +60,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
